7c016124d009235b053cba0f17968c29f9630e4c
unagi22 <djordjedabetic22@gmail.com>
2023-09-08 12:54:18 +0200

    code update

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
-let list = document.getElementById("items-list");
-let form = document.getElementsByTagName("form")[0];
-let items = list.getElementsByClassName("btn-delete");
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const searchInput = document.getElementById('searchInput');
+const searchResults = document.getElementById('searchResults');
 
-function addItem(event) {
-  event.preventDefault();
-  let inputText = document.getElementById("input-text").value;
-  if (!!inputText) {
-    document.getElementById("items-list").innerHTML +=
-      "<li class='item'>" +
-      inputText +
-      "<button class='btn btn-delete'>X</button></li>";
-  }
+let tasks = [];
+let originalTasks = [];
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        tasks.push(taskText);
+        originalTasks.push(taskText);
+        renderTasks();
+        taskInput.value = '';
+    }
 }
 
-document.querySelector("input[type=submit]").addEventListener("click", addItem);
-
-let deleteButtons = document.querySelectorAll(".btn-delete");
-for (let btn of deleteButtons) {
-  btn.addEventListener("click", function deleteItem() {
-    btn.parentElement.remove();
-  });
+function renderTasks() {
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            ${task}
+            <button onclick="removeTask(${index})">Remove</button>
+        `;
+        taskList.appendChild(li);
+    });
 }
+
+function removeTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+}
+
+function searchTasks() {
+    const searchText = searchInput.value.trim().toLowerCase();
+
+    if (searchText === '') {
+        // Clear search results when the search input is empty
+        searchResults.style.display = 'none';
+        tasks = [...originalTasks]; // Restore original tasks
+        renderTasks();
+    } else {
+        const filteredTasks = tasks.filter(task => task.toLowerCase().includes(searchText));
+
+        if (filteredTasks.length === 0) {
+            searchResults.style.display = 'none';
+        } else {
+            searchResults.style.display = 'block';
+            searchResults.innerHTML = '';
+
+            filteredTasks.forEach(task => {
+                const resultItem = document.createElement('div');
+                resultItem.textContent = task;
+                resultItem.addEventListener('click', () => {
+                    // Keep only the selected item and re-render the list
+                    tasks = [task];
+                    renderTasks();
+                    searchInput.value = task;
+                    searchResults.style.display = 'none';
+                });
+                searchResults.appendChild(resultItem);
+            });
+        }
+    }
+}
+
+searchInput.addEventListener('focus', searchTasks);
+searchInput.addEventListener('input', searchTasks);
